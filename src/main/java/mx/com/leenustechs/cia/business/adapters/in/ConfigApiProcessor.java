8c1d95e5bcaa23090e2f7c449be0561feab65ffe
Renee <reneleermz_@live.com.mx>
2026-07-22 02:05:43 -0600

    FIX NEW WAY TO HANDLE PROCESS

---
+++ src/main/java/mx/com/leenustechs/cia/business/adapters/in/ConfigApiProcessor.java
@@ -0,0 +1,55 @@
+package mx.com.leenustechs.cia.business.adapters.in;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mx.com.leenustechs.cia.business.services.OperationTypeService;
+import mx.com.leenustechs.cia.business.utils.mappers.CommonModelMapper;
+import mx.com.leenustechs.cia.models.CommonModel;
+import mx.com.leenustechs.cia.models.requests.CommonModelRequest;
+import mx.com.leenustechs.cia.models.responses.CommonModelResponse;
+import mx.com.leenustechs.cia.models.types.OperationType;
+import tools.jackson.databind.node.JsonNodeFactory;
+import tools.jackson.databind.node.ObjectNode;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ConfigApiProcessor {
+
+    private static final String PRODUCER = "CONFIG_API";
+    private final OperationTypeService operationTypeService;
+    private final CommonModelMapper commonModelMapper;
+
+    public CommonModelResponse processLogin(CommonModelRequest requestEvent, HttpServletRequest request) {
+        String transactionId = (String) request.getAttribute("transactionId");
+
+        CommonModel event = commonModelMapper.toModel(
+                requestEvent,
+                transactionId,
+                PRODUCER,
+                OperationType.LOGIN);
+
+        CommonModelResponse response = event.getCommand().execute(event, operationTypeService);
+
+        if (response != null) {
+            return response;
+        }
+
+        return buildNoResponseError(transactionId);
+    }
+
+    private CommonModelResponse buildNoResponseError(String transactionId) {
+        ObjectNode errorPayload = JsonNodeFactory.instance.objectNode();
+
+        errorPayload.put("error", "No response from operation");
+
+        return new CommonModelResponse(
+                transactionId,
+                PRODUCER,
+                OperationType.LOGIN,
+                errorPayload);
+    }
+}
--- src/main/java/mx/com/leenustechs/cia/rest/ConfigApi.java
+++ src/main/java/mx/com/leenustechs/cia/rest/ConfigApi.java
@@ -2,29 +2,21 @@ package mx.com.leenustechs.cia.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import mx.com.leenustechs.cia.business.services.OperationTypeService;
-import mx.com.leenustechs.cia.business.utils.mappers.CommonModelMapper;
-import mx.com.leenustechs.cia.models.CommonModel;
+import mx.com.leenustechs.cia.business.adapters.in.ConfigApiProcessor;
 import mx.com.leenustechs.cia.models.requests.CommonModelRequest;
 import mx.com.leenustechs.cia.models.responses.CommonModelResponse;
-import mx.com.leenustechs.cia.models.types.OperationType;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.databind.node.JsonNodeFactory;
-import tools.jackson.databind.node.ObjectNode;
 
 @RestController
 @RequestMapping("/config-api/v1/activation")
 @RequiredArgsConstructor
 public class ConfigApi {
 
-    private static final String PRODUCER = "CONFIG_API";
-
-    private final OperationTypeService operationTypeService;
-    private final CommonModelMapper commonModelMapper;
+    private final ConfigApiProcessor configApiProcessor;
 
     @PostMapping(
             value = "/login",
@@ -35,36 +27,6 @@ public class ConfigApi {
             @RequestBody CommonModelRequest requestEvent,
             HttpServletRequest request
     ) {
-        String transactionId = (String) request.getAttribute("transactionId");
-
-        CommonModel event = commonModelMapper.toModel(
-                requestEvent,
-                transactionId,
-                PRODUCER,
-                OperationType.LOGIN
-        );
-
-        CommonModelResponse response =
-                event.getCommand().execute(event, operationTypeService);
-
-        if (response != null) {
-            return response;
-        }
-
-        return buildNoResponseError(transactionId);
-    }
-
-    private CommonModelResponse buildNoResponseError(String transactionId) {
-        ObjectNode errorPayload =
-                JsonNodeFactory.instance.objectNode();
-
-        errorPayload.put("error", "No response from operation");
-
-        return new CommonModelResponse(
-                transactionId,
-                PRODUCER,
-                OperationType.LOGIN,
-                errorPayload
-        );
+        return configApiProcessor.processLogin(requestEvent, request);
     }
 }
