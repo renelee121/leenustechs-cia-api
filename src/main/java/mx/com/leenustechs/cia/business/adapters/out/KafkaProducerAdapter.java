dbbecad0ebdb16ebf82962d853929842bb57ba62
Renee <reneleermz_@live.com.mx>
2026-07-22 11:36:07 -0600

    FIX KAFKA PUBLISH

--- src/main/java/mx/com/leenustechs/cia/business/adapters/out/KafkaProducerAdapter.java
+++ src/main/java/mx/com/leenustechs/cia/business/adapters/out/KafkaProducerAdapter.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j
@@ -11,7 +14,20 @@ import org.springframework.kafka.core.KafkaTemplate;
 @RequiredArgsConstructor
 public class KafkaProducerAdapter {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void publish(String topic, String key, Object event){}
+    public void publish(String topic, String key, Object event){
+        log.info("Publishing event to Kafka topic: {}, key: {}, event: {}", topic, key, event);
+        ProducerRecord<String, Object> record = new ProducerRecord<>(topic, key, event);
+        record.headers().add("origin-service", applicationName.getBytes());
+        try{
+            kafkaTemplate.send(record);
+        }catch(Exception e){
+            log.error("Error publishing event to Kafka topic: {}, key: {}, event: {}, error: {}", topic, key, event, e.getMessage(), e);
+            throw e;
+        }
+    }
 }
